7b25f44deb6928720a4592d91860b0a332eb60db
Ryan Johnson <ryjoh@stanford.edu>
2025-04-30 15:49:55 -0700

    post processor

---
+++ Wang/nn_chemistry/standalone_nn/post.py
@@ -0,0 +1,31 @@
+#!python3
+import numpy as np
+import matplotlib.pyplot as plt
+import cantera as ct
+
+gas = ct.Solution("FFCM2_H2_Ar.yaml")
+dat = np.loadtxt("out_file.txt", delimiter=',')
+
+temp = 2500
+pres = 2
+spec = "H2:2, O2:1, AR:7"
+gas.TPX = temp, pres * ct.one_atm, spec
+r = ct.IdealGasReactor(gas)
+sim = ct.ReactorNet([r])
+t_end = 10000 * 1e-9
+states = ct.SolutionArray(gas, extra=["time"])
+while sim.time < t_end:
+    sim.step()
+    states.append(r.thermo.state, time=sim.time)
+
+time = dat[:,0]
+T = dat[:,1]
+
+plt.plot(time, T,'-k')
+plt.plot(states.time, states.T,'--r')
+
+for k, specie in enumerate(gas.species_names):
+    plt.figure()
+    plt.plot(time, dat[:, 3 + k], '-k')
+    plt.plot(states.time, states.concentrations[:,k], '--r')
+plt.show()
--- Wang/nn_chemistry/standalone_nn/test.cpp
+++ Wang/nn_chemistry/standalone_nn/test.cpp
@@ -23,17 +23,17 @@
 
 //clang++ -std=c++17  -O3 -o nn_chemgen_test
 // Overload << operator for std::array
-
 template <typename T, std::size_t N>
 std::ostream& operator<<(std::ostream& os, const std::array<T, N>& arr) {
-    os << "[ ";
-    for (const auto& value : arr) 
-    {
-        os << value << " ";
+    for (std::size_t i = 0; i < N; ++i) {
+        os << arr[i];
+        if (i != N - 1) {
+            os << ", ";
+        }
     }
-    os << "]";
     return os;
 }
+
 
 int main() {
     std::cout << "*** ChemGen ***" <<std::endl;
@@ -59,16 +59,49 @@ int main() {
     double pressure_ = 101325.0 * 2.0;
 
     Species concentrations = concentrations_from_molefractions_pressure_temperature(molefractions_, pressure_, temperature_);
-    std::cout << concentrations << std::endl;
+    
     std::array<double, 10> input  = {};
     input[0] = temperature_;
     input[1] = pressure_/101325.0;
+
+    double dt = 1e-9;
 
     for(int i = 0; i<n_species - 1; i++)
     {
         input[2 + i] = concentrations[i];
     }
-    std::cout << input << std::endl;
+
+    std::cout<< 0.0<<", "<<temperature_ << ", " << pressure_/101325.0 << ", "<<concentrations <<std::endl;
+    double time  = 0;
+    for (int  i  = 0; i< 10000; i++)
+    {
+        auto model_output = NN16<Scalar>(input); // Change input to desired features
+        Species source_ = source(concentrations, temperature_);
+
+        std::array<Scalar, n_species - 1> output_species;
+        for (int i = 0; i < n_species - 1; i++) {
+            output_species[i] = model_output[i + 2] + input[i + 2] + source_[i] * dt; // NN outputs change of state properties, transferred to real values
+        }
+
+        for(int i = 0; i<n_species - 1; i++)
+        {
+            concentrations[i] = output_species[i];
+        }
+        
+        temperature_ = model_output[0] + input[0];
+        pressure_ = model_output[1] + input[1];
+
+        input[0] = temperature_;
+        input[1] = pressure_;
+        for(int i = 0; i<n_species - 1; i++)
+        {
+            input[2 + i] = concentrations[i];
+        }
+        time+=dt;
+        std::cout<< time<<", "<<temperature_ << ", " << pressure_ << ", "<<concentrations <<std::endl;
+
+    }
+
 
     //// First loop: NN16 + source
     //auto start1 = std::chrono::high_resolution_clock::now();
