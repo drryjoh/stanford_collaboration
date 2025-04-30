79f4c23b52605d5318e390beb162a1f065fe432b
Ryan Johnson <ryjoh@stanford.edu>
2025-04-30 15:13:24 -0700

    updated NN16

--- Wang/nn_chemistry/standalone_nn/test.cpp
+++ Wang/nn_chemistry/standalone_nn/test.cpp
@@ -22,31 +22,73 @@
 #include <chrono>
 
 //clang++ -std=c++17  -O3 -o nn_chemgen_test
+// Overload << operator for std::array
+
+template <typename T, std::size_t N>
+std::ostream& operator<<(std::ostream& os, const std::array<T, N>& arr) {
+    os << "[ ";
+    for (const auto& value : arr) 
+    {
+        os << value << " ";
+    }
+    os << "]";
+    return os;
+}
 
 int main() {
     std::cout << "*** ChemGen ***" <<std::endl;
-    Species mass_fractions  = {double(0.0),double(0.0),double(0.0),double(0.006770331319280119),double(0.0),double(0.0),double(0.0),double(0.0),double(0.0)};
-    double temperature_ =  2000.0;
-    double pressure_ = 101325.0;
-    std::array<double, 10> input_real  = {};
-    // First loop: NN16 + source
-    auto start1 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 100000; i++) {
-        NN16(input_real);
-        source(mass_fractions, temperature_);
-    }
-    auto end1 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed1 = end1 - start1;
-    std::cout << "Loop 1 (NN16 + source): " << elapsed1.count() << " seconds\n";
+    /*
+    temp = 2500
+    pres = 2
+    spec = "H2:2, O2:1, AR:7"
+    return {"H", "H2", "O", "O2", "OH", "H2O", "HO2", "H2O2", "AR"};
+    */
+    Species molefractions_  = 
+    {double(0),
+     double(2),
+     double(0),
+     double(1),
+     double(0),
+     double(0),
+     double(0),
+     double(0),
+     double(7)};
+     molefractions_ = scale_gen(double(0.1), molefractions_);
 
-    // Second loop: NN16 only
-    auto start2 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 100000; i++) {
-        NN16(input_real);
+    double temperature_ =  2500.0;
+    double pressure_ = 101325.0 * 2.0;
+
+    Species concentrations = concentrations_from_molefractions_pressure_temperature(molefractions_, pressure_, temperature_);
+    std::cout << concentrations << std::endl;
+    std::array<double, 10> input  = {};
+    input[0] = temperature_;
+    input[1] = pressure_/101325.0;
+
+    for(int i = 0; i<n_species - 1; i++)
+    {
+        input[2 + i] = concentrations[i];
     }
-    auto end2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed2 = end2 - start2;
-    std::cout << "Loop 2 (NN16 only): " << elapsed2.count() << " seconds\n";
+    std::cout << input << std::endl;
+
+    //// First loop: NN16 + source
+    //auto start1 = std::chrono::high_resolution_clock::now();
+    //for (int i = 0; i < 100000; i++) 
+    //{
+    //    output  = input + NN16(input);
+    //    source(mass_fractions, temperature_);
+    //}
+    //auto end1 = std::chrono::high_resolution_clock::now();
+    //std::chrono::duration<double> elapsed1 = end1 - start1;
+    //std::cout << "Loop 1 (NN16 + source): " << elapsed1.count() << " seconds\n";
+
+    //// Second loop: NN16 only
+    //auto start2 = std::chrono::high_resolution_clock::now();
+    //for (int i = 0; i < 100000; i++) {
+    //    NN16(input_real);
+    //}
+    //auto end2 = std::chrono::high_resolution_clock::now();
+    //std::chrono::duration<double> elapsed2 = end2 - start2;
+    //std::cout << "Loop 2 (NN16 only): " << elapsed2.count() << " seconds\n";
     return 0;
 }
             
