c0992e31cd15e7da06bb44384d525f5d9447242f
Ryan Johnson <ryjoh@stanford.edu>
2025-05-22 18:40:07 -0700

    remove Y Eqn solve per species

--- Wang/nn_chemistry/detonationRRCompare/detonationEulerFoam.C
+++ Wang/nn_chemistry/detonationRRCompare/detonationEulerFoam.C
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]) {
         #include "rhoEqn.H"
         #include "rhoUEqn.H"
         #include "rhoYEqn.H"
-        #include "rhoEEqn.H"
+        //#include "rhoEEqn.H"
         scalar time_after = timer.elapsedTime();
         entire_time += time_after - time_before;
         Info << "Time to solve iterations complete system:          "<< time_after-time_before <<endl;
