9b500868f59a7649419bb828ce433182db97fb59
Josh Lipman <jelipman@stanford.edu>
2025-08-21 16:49:35 -0700

    updates to znd generator

--- Wang/nn_chemistry/C2H4_Detonatoin/znd_ethylene_solution_generator.py
+++ Wang/nn_chemistry/C2H4_Detonatoin/znd_ethylene_solution_generator.py
@@ -6,7 +6,34 @@ import cantera as ct
 import numpy as np
 import csv 
 
-#Set gas properties and mechansim
+def run_znd_safe(gas, gas1, cj_speed, t_end):
+    tried = []
+    for Method, (relTol, absTol) in [
+        ('LSODA', (1e-8, 1e-12)),
+        ('Radau', (1e-8, 1e-12)),   # another stiff solver
+        ('BDF',   (1e-8, 1e-12)),   # keep, but not hyper-tight
+    ]:
+        try:
+            tried.append((Method, relTol, absTol))
+            return zndsolve(
+                gas, gas1, cj_speed, t_end=t_end,
+                advanced_output=True, Method=Method,
+                relTol=relTol, absTol=absTol
+            )
+        except Exception as e:
+            print(f"[WARN] zndsolve failed with {Method} (relTol={relTol}, absTol={absTol}): {e}")
+    raise RuntimeError(f"All ZND attempts failed. Tried: {tried}")
+
+
+# ---------------- CLEANUP ----------------
+def cleanup(arr, tol=1e-20):
+    arr = np.nan_to_num(arr, nan=0.0, posinf=1e300, neginf=0.0)
+    arr[arr < tol] = 0.0
+    return arr
+# -----------------------------------------
+
+
+#Set gas properties and mechanism
 P = 0.2 * 101325
 T = 298
 X = 'C2H4:1,O2:3,N2:4' #Should be argon, but for now just testing to get working in OF
@@ -29,7 +56,9 @@ cj_speed, R2, plot_data = CJspeed(P, T, X, './mechanism/ffcmy9reduced30.yaml', f
 
 gas = PostShock_fr(cj_speed, P, T, X, './mechanism/ffcmy9reduced30.yaml')
 
-znd_out = zndsolve(gas,gas1,cj_speed,t_end=1e-3,advanced_output=True,Method='BDF',relTol=1e-20,absTol=1e-18)
+znd_out = run_znd_safe(gas, gas1, cj_speed, t_end=1e-3)
+
+
 
 species_names_array = znd_out['gas1'].species_names
 n_species = len(species_names_array)
@@ -37,28 +66,24 @@ n_cols = 4+1+n_species #pressure, density, temperature, velocity, X (additional 
 header = ['X','Pressure', 'Temperature', 'Density','Velocity'] + species_names_array
 
 # extract distance from solution 
-x_array = znd_out['distance']
-x_array = np.transpose(x_array)
+x_array = cleanup(np.transpose(znd_out['distance']))
 
 #extract pressure from solution
-p_array = znd_out['P']
-p_array = np.transpose(p_array)
+p_array = cleanup(np.transpose(znd_out['P']))
 
 #extract temperature from solution
-T_array = znd_out['T']
-T_array = np.transpose(T_array)
+T_array = cleanup(np.transpose(znd_out['T']))
 
 #extract density from solution
-rho_array = znd_out['rho']
-rho_array = np.transpose(rho_array)
+rho_array = cleanup(np.transpose(znd_out['rho']))
 
-#extract density from solution
+#extract velocity from solution
 U1 = np.array(znd_out['U1'])
 U = np.array(znd_out['U'])
-vel_array = U1 - U #NOTE: Changing relative coordinates
+vel_array = cleanup(U1 - U) #NOTE: Changing relative coordinates
 
 #extract species from solution
-species_array = znd_out['species'].T
+species_array = cleanup(znd_out['species'].T)
 
 #create output array
 out_array = np.zeros((len(x_array),n_cols))
@@ -71,11 +96,11 @@ out_array[:,2] = T_array
 out_array[:,3] = rho_array
 out_array[:,4] = vel_array
 out_array[:,5:] = species_array
+
 with open(file_name+'_ZND_solution.csv', 'w') as f:
-# with open(file_name+'_'+str(cj_speed)+'overdriven_zndshk_solution.txt', 'w') as f:
     for name in header:
         f.write('"%s" '%name)
     f.write('\n')
+
 with open(file_name+'_ZND_solution.csv', 'a') as f:
-# with open(file_name+'_'+str(cj_speed)+'overdriven_zndshk_solution.txt', 'a') as f:
     csv.writer(f, delimiter=' ').writerows(out_array)
