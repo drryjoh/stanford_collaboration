cbf204b83432b55de82e462aa7c8f83d796a33ff
Josh Lipman <jelipman@stanford.edu>
2025-09-01 09:58:53 -0700

    update chemgen.h

--- Wang/nn_chemistry/detonationFoamNavierStokesSdt_modified_ethylene_23/chemgen/chemgen.h
+++ Wang/nn_chemistry/detonationFoamNavierStokesSdt_modified_ethylene_23/chemgen/chemgen.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <cmath>
 #include <algorithm>
 #include <array>
@@ -20,10 +19,10 @@
 #include "reactions.h"
 #include "source.h"
 #include "chemical_state_functions.h"
+#include "default_parameters.h"
 #include "rk4.h"
 #include "linear_solvers.h"
-#include "backwards_euler.h"
+#include "backward_euler.h"
 #include "sdirk.h"
 #include "rosenbroc.h"
 #include "yass.h"
-
