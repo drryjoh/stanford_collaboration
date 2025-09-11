7fca2fd1d30aaf4b103b3d74d026bd49a68f89d3
Josh Lipman <jelipman@stanford.edu>
2025-09-10 20:58:57 -0700

    znd generator update conditions

--- Wang/nn_chemistry/ZND_Init/znd_solution_generator.py
+++ Wang/nn_chemistry/ZND_Init/znd_solution_generator.py
@@ -7,11 +7,11 @@ import numpy as np
 import csv 
 
 #Set gas properties and mechansim
-P = 6670
+P = 40000
 T = 298
-X = 'H2:2,O2:1,AR:7'
+X = 'H2:2,O2:1,N2:3.76'
 
-gas1 = ct.Solution('FFCM2_H2_Ar.yaml')
+gas1 = ct.Solution('FFCM2_H2_N2.yaml')
 gas1.TPX = T, P, X
 
 
@@ -25,9 +25,9 @@ for name, Y in zip(species, mass_fractions):
 print(f"Density: {gas1.density:.4f} kg/m³")
 
 
-cj_speed, R2, plot_data = CJspeed(P, T, X, 'FFCM2_H2_Ar.yaml', fullOutput=True)
+cj_speed, R2, plot_data = CJspeed(P, T, X, 'FFCM2_H2_N2.yaml', fullOutput=True)
 print("CJ SPEED:  ", cj_speed)
-gas = PostShock_fr(cj_speed, P, T, X, 'FFCM2_H2_Ar.yaml')
+gas = PostShock_fr(cj_speed, P, T, X, 'FFCM2_H2_N2.yaml')
 
 znd_out = zndsolve(gas,gas1,cj_speed,t_end=1e-3,advanced_output=True,Method='BDF',relTol=1e-20,absTol=1e-18)
 
@@ -63,7 +63,7 @@ species_array = znd_out['species'].T
 #create output array
 out_array = np.zeros((len(x_array),n_cols))
 
-file_name = 'ffcm2methane'
+file_name = 'ffcm2hydrogenair'
 #stack things up here
 out_array[:,0] = x_array
 out_array[:,1] = p_array
