0d523837ce2c3cd117f177ba5df15f155f7c1349
Josh Lipman <jelipman@stanford.edu>
2025-09-24 20:52:17 -0700

    no message

--- Wang/nn_chemistry/Chemgen_cases/Chemgen_timings/timings_after_fluid.h
+++ Wang/nn_chemistry/Chemgen_cases/Chemgen_timings/timings_after_fluid.h
@@ -2,11 +2,11 @@
     scalar fluid_time_rank = time_after_fluid_rank - time_before_fluid_rank;
 
 
-    fluid_time = reduce(fluid_time_rank, sumOp<scalar>()) - chemistry_time;
+    scalar fluid_time = reduce(fluid_time_rank, sumOp<scalar>()) - chemistry_time;
     running_fluid_time += fluid_time;
 
     Info << "Time for entire fluid solve:     " << fluid_time;
     Info << "Time for entire chemistry solve: " << chemistry_time;
     Info << "Running fluid solve time       : " << running_fluid_time;
-        Info << "Running chemistry solve time   : " << running_chemistry_time;
+    Info << "Running chemistry solve time   : " << running_chemistry_time;
     Info << "Chemistry Cost   :               " << running_chemistry_time/(running_fluid_time + running_chemistry_time);
